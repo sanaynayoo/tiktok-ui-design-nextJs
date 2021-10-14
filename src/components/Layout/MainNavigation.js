db3c7c4c198f9f127d0e162afde2c5fbf15bba99
Theo Nay Oo <theonayoo@Theos-MacBook-Air.local>
2021-10-14 21:19:08 +0630

    fix css

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -17,35 +17,33 @@ const MainNavigation = () => {
         </Link>
       </div>
 
-      <div className={styles.searchContainer}>
+      <div className={styles.inputContainer}>
         <input placeholder="Search accounts and videos" />
         <button>
           <Image
             src="/static/icons/search.svg"
             alt="search icon"
             width={20}
-            height={20}
+            height={18}
           />
         </button>
       </div>
 
       <div className={styles.uploadContainer}>
-        <Link href="/Upload">
-          <a>Upload</a>
-        </Link>
+        <div className={styles.uploadContent}>
+          <Link href="/Upload">Upload</Link>
+        </div>
         <button className={styles.loginContent}>
-          <Link href="/Login">
-            <a>Login</a>
-          </Link>
+          <Link href="/Login">Login</Link>
         </button>
-        {/* <span> */}
-        <Image
-          src="/static/icons/more.svg"
-          alt="search icon"
-          width={20}
-          height={20}
-        />
-        {/* </span> */}
+        <div className={styles.moreContent}>
+          <Image
+            src="/static/icons/more.svg"
+            alt="search icon"
+            width={20}
+            height={20}
+          />
+        </div>
       </div>
     </div>
   );
